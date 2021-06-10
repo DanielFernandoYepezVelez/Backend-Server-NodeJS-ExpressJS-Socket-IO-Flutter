de44337183fe80a811beae73ea9e9305e0649ddb
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-06-10 18:39:11 -0500

    Agregando Modelos De Bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,10 +1,23 @@
 /* Aqui Tengo La Configuación De Socket.io */
 const { io } = require('../index');
 
+const Bands = require('../models/Bands');
+const Band = require('../models/Band');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Heroes Del Silencio'));
+bands.addBand(new Band('Metalica'));
+// console.log(bands);
+
 /* Mensajes De Sockets */
 /* Todos Los Clientes Que Se Conecten A Este Backend Server Escucharán Estos Mensajes */
 io.on('connection', client => {
     console.log('Cliente Conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente Desconectado');
@@ -12,8 +25,13 @@ io.on('connection', client => {
 
     client.on('mensaje', (payload) => {
         console.log('mensaje: ', payload);
-
         io.emit('mensaje', { admin: 'Mensaje RECIBIDO Desde El Frontend' });
+    });
+
+    client.on('emitir-mensaje', (payload) => {
+        console.log(payload);
+        io.emit('nuevo-mensaje', payload); // Emite Para Todo El Mundo E Incluso El Que Lo Emitio
+        // client.broadcast.emit('Nuevo Mensaje Emitido Backend', 'Heyy!!!'); // Emite Para Todo El Mundo Excepto El Que Lo Emitio
     });
 
 });
